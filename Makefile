f6fa5fe1a4d31cb5f8d49a26abac1f36a4b3e681
Stephen Benjamin <stephen@redhat.com>
2024-09-27 07:48:00 -0400

    Migrate version

--- Makefile
+++ Makefile
@@ -4,9 +4,9 @@ GIT_COMMIT := $(shell git rev-parse --short HEAD)
 BUILD_DATE := $(shell date -u +'%Y-%m-%dT%H:%M:%SZ')
 GIT_TREE_STATE := $(shell if git diff --quiet; then echo clean; else echo dirty; fi)
 
-LDFLAGS := -X '$(GO_PKG_NAME)/pkg/extensions.CommitFromGit=$(GIT_COMMIT)' \
-           -X '$(GO_PKG_NAME)/pkg/extensions.BuildDate=$(BUILD_DATE)' \
-           -X '$(GO_PKG_NAME)/pkg/extensions.GitTreeState=$(GIT_TREE_STATE)'
+LDFLAGS := -X '$(GO_PKG_NAME)/pkg/version.CommitFromGit=$(GIT_COMMIT)' \
+           -X '$(GO_PKG_NAME)/pkg/version.BuildDate=$(BUILD_DATE)' \
+           -X '$(GO_PKG_NAME)/pkg/version.GitTreeState=$(GIT_TREE_STATE)'
 
 all: test build
 
--- pkg/extensions/extension.go
+++ pkg/extensions/extension.go
@@ -1,21 +1,13 @@
 package extensions
 
-var (
-	// CommitFromGit is a constant representing the source version that
-	// generated this build. It should be set during build via -ldflags.
-	CommitFromGit string
-	// BuildDate in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
-	BuildDate string
-	// GitTreeState has the state of git tree, either "clean" or "dirty"
-	GitTreeState string
-)
+import "github.com/openshift-eng/openshift-tests-extension/pkg/version"
 
 var DefaultExtension = Extension{
 	APIVersion: "v1",
 	Source: Source{
-		Commit:       CommitFromGit,
-		BuildDate:    BuildDate,
-		GitTreeState: GitTreeState,
+		Commit:       version.CommitFromGit,
+		BuildDate:    version.BuildDate,
+		GitTreeState: version.GitTreeState,
 		SourceURL:    "https://github.com/openshift-eng/openshift-tests-extension",
 	},
 	Component: Component{
